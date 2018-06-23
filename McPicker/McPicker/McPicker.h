2c0f5c9555f7bd50d62920daaeb1c67fa4681720
Ken Cox <kenstir@gmail.com>
2018-06-23 14:57:48 -0400

    take 2: xcode project from scratch

---
+++ McPicker/McPicker/McPicker.h
@@ -0,0 +1,18 @@
+//
+//  McPicker.h
+//  McPicker
+//
+//  Created by Ken Cox on 6/23/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for McPicker.
+FOUNDATION_EXPORT double McPickerVersionNumber;
+
+//! Project version string for McPicker.
+FOUNDATION_EXPORT const unsigned char McPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <McPicker/PublicHeader.h>
+
+
---
+++ McPicker/McPickerTests/McPickerTests.swift
@@ -0,0 +1,35 @@
+//
+//  McPickerTests.swift
+//  McPickerTests
+//
+//  Created by Ken Cox on 6/23/18.
+//
+
+import XCTest
+@testable import McPicker
+
+class McPickerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

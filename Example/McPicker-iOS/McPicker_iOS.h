5c4325d6c8b3eb2ed9665839ef59606523cb9274
Ken Cox <kenstir@gmail.com>
2018-06-23 13:33:15 -0400

    Use framework target

---
+++ Example/McPicker-iOS/McPicker_iOS.h
@@ -0,0 +1,19 @@
+//
+//  McPicker_iOS.h
+//  McPicker-iOS
+//
+//  Created by Ken Cox on 6/23/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for McPicker_iOS.
+FOUNDATION_EXPORT double McPicker_iOSVersionNumber;
+
+//! Project version string for McPicker_iOS.
+FOUNDATION_EXPORT const unsigned char McPicker_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <McPicker_iOS/PublicHeader.h>
+
+
